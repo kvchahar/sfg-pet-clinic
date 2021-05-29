18e8e243fe80319a52fe30afcbdc99f242fb22d9
This Pc <krishnaveer.chahar_cs19@gla.ac.in>
2021-05-29 08:30:19 +0530

    adding Vet list and updated controller. closes #15

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -39,7 +39,7 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
+        vet1.setId(3L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
